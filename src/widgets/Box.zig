8933b54ed51c914fbe05fd759ac1c6acee82d754
Maximilian <github@maximilian-heidenreich.de>
2026-02-26 01:44:55 +0100

    refac: make Box NestedView

--- src/widgets/Box.zig
+++ src/widgets/Box.zig
@@ -1,5 +1,8 @@
 //!
-//!
+//! A generic box container with possible children.
+//! Allows for easy containment of sub-views and acces
+//! to apply styles to all children through the `.style`
+//! option. Basically like an HTML `<div>`.
 //!
 
 const Box = @This();
@@ -7,11 +10,14 @@ const Opts = struct {
     title: []const u8 = "",
 };
 
-pub fn init(opts: ViewOpts(Opts)) View(Box) {
+pub fn init(
+    children: anytype,
+    opts: ViewOpts(Opts),
+) NestedView(Box, @TypeOf(children)) {
     // TODO(views): Look into whether we want to support
     // overriding i.e. a custom .style here directly
     // if so we migth need some `merge(.{}, opts)` thing.
-    return View(Box).init(opts);
+    return NestedView(Box, @TypeOf(children)).init(opts);
 }
 
 pub fn draw(self: Box, ctx: Ctx, container: RectU) void {
@@ -28,4 +34,5 @@ const RectU = M.math.RectU;
 const Ctx = M.views.Ctx;
 const CommonViewOpts = M.views.CommonViewOpts;
 const View = M.views.View;
+const NestedView = M.views.NestedView;
 const ViewOpts = M.views.ViewOpts;
