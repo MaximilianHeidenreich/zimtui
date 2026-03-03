a9a5b90b0c6e0ce4d91df361d3a0fa87d699d20f
Maximilian <github@maximilian-heidenreich.de>
2026-03-03 17:50:55 +0100

    example: chores

--- examples/main.zig
+++ examples/main.zig
@@ -2,9 +2,18 @@ const std = @import("std");
 const zimtui = @import("zimtui");
 const TUI = zimtui.TUI;
 const Box = zimtui.views.Box;
+const Text = zimtui.views.Text;
+const Label = zimtui.views.Label;
 
 pub fn main() !void {
     var tui = try TUI.init(std.heap.smp_allocator, .{});
     // var app = App.init();
-    try tui.run(Box(.{}, .{ .size = .fixed(2) }));
+    // try tui.run(Box(.{}, .{ .size = .fixed(2) }));
+    // try tui.run(Label("Hello Kek", .{ .size = .fixed(2) }));
+    try tui.run(
+        Box(
+            Text("Hello: {s}", .{"Fmt!"}, .{}),
+            .{ .style = .{ .bg = .{ .indexed = .grey_82 } } },
+        ),
+    );
 }
